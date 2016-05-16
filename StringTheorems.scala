7c30faee0ebb11886b570bf13076095f2731f72d
Mikaël Mayer <MikaelMayer@users.noreply.github.com>
2016-05-16 11:27:39 +0200

    Made the lemma for extraction to work !

--- StringTheorems.scala
+++ StringTheorems.scala
@@ -12,49 +12,24 @@ object StringTest {
     case Zero => c
   }
   
-  def LemmaEquationSplit(A: String, B: String, C: String, D: String) = {
-    require(A + B == C + D && A.bigLength == C.bigLength)
-    A == C && B == D
-  }.holds
-  
-  def LemmaInequalitySplit(p: String, q: String) = {
-    p.bigLength < q.bigLength || p.bigLength == q.bigLength || p.bigLength > q.bigLength
-  }.holds
-  
-  def stripPrefix(s: String, i: BigInt): String = {
-    s.bigSubstring(i)
-  }
-  /*3) prefix-introduce (similar for suffix-introduce)
-| p |`< | q |  && p A = q B
-<=>
-There exist a constant k such that q = p k and A = k B*/
-  def LemmaPrefixIntroduce(p: String, q: String, A: String, B: String) = {
-    require(p.bigLength < q.bigLength && p + A == q + B)
-    val k = stripPrefix(q, p.bigLength)
-    k
-  } ensuring { (k: String) => q == p + k && A == k + B }
-  
-  def LemmaCommutation1(A: String, B: String, C: String) = {
-    require(A + B == C + A && C.bigLength >= A.bigLength)
-    val k2 = stripPrefix(C, A.bigLength)
-    val k1 = A
-    (k1, k2)
+  // Splits the string at a given index.
+  def split(s: String, i: BigInt): (String, String) = {
+    require(i >= 0 && i <= s.bigLength)
+    (s.bigSubstring(0, i), s.bigSubstring(i))
   } ensuring {
-    k => C == k._1 + k._2 && B == k._2 + k._1 && A == k._1
+    res => res._1 + res._2 == s && res._1.bigLength == i && res._2.bigLength == s.bigLength - i
   }
   
-  def LemmaCommutation2(A: String, B: String, C: String): (String, String) = {
-    require(A + B == C + A)
-    if(C.bigLength < A.bigLength) {
-      val Ap = stripPrefix(A, C.bigLength)
-      LemmaCommutation2(Ap, B, C)
-    } else {
-      LemmaCommutation1(A, B, C)
-    }
+  // Splits the string at a given index starting from the end
+  def splitFromEnd(s: String, i: BigInt): (String, String) = {
+    require(i >= 0 && i <= s.bigLength)
+    val j = s.bigLength - i
+    split(s, j)
   } ensuring {
-    k => C == k._1 + k._2 && B == k._2 + k._1 // Maybe something on A
+    res => res._1 + res._2 == s && res._1.bigLength == s.bigLength - i && res._2.bigLength == i
   }
   
+  // Computes A + A + ... + A,  n times */
   def power(A: String, n: BigInt): String = {
     require(n >= 0)
     if(n == 0) ""
@@ -62,15 +37,133 @@ There exist a constant k such that q = p k and A = k B*/
     else A + power(A, n-1)
   }
   
-  def LemmaAssociative(A: String, B: String, C: String): Boolean = {
+  /** A + B == C + D and |A| == |C| <==> B == D && A == C */
+  def Lemma001EquationSplit(A: String, B: String, C: String, D: String) = {
+    require(A + B == C + D && A.bigLength == C.bigLength)
+    A == C && B == D
+  }.holds
+  
+  def Lemma002InequalitySplit(p: String, q: String) = {
+    p.bigLength < q.bigLength || p.bigLength == q.bigLength || p.bigLength > q.bigLength
+  }.holds
+  
+  // If a string right-commutes with C to yield B on the left, B and C have the same size.
+  def Lemma003Stringsizes(A: String, B: String, C: String) = {
+    require(A + B == C + A)
+    C.bigLength == B.bigLength
+  } holds
+  
+  
+  // Associativity
+  def LemmaAssociativity(A: String, B: String, C: String): Boolean = {
     (A + B) + C == A + (B + C)
   } holds
   
-  def LemmaPower2OrMore(A: String, n: BigInt) = {
-    require(n > 1)
-    power(A, n) == A + power(A, n-1)
+  // Left simplification
+  def LemmaLeftSimplification(A: String, B: String, C: String): Boolean = {
+    require(C + A == C + B)
+    A == B
+  } holds
+  
+  // Right simplification
+  def LemmaRightSimplification(A: String, B: String, C: String): Boolean = {
+    require(A + C == B + C)
+    A == B
+  } holds
+  
+  /*3) prefix-introduce
+| p |`< | q |  && p A = q B
+<=>
+There exist a constant k such that q = p k and A = k B*/
+  def Lemma005PrefixIntroduce(p: String, q: String, A: String, B: String) = {
+    require(p.bigLength < q.bigLength && p + A == q + B)
+    val (l, k) = split(q, p.bigLength)
+    k
+  } ensuring { (k: String) => q == p + k && A == k + B }
+  
+  /*3bis) suffix-introduce
+| p |`< | q |  && A p = B q
+<=>
+There exist a constant k such that q = k p and A = B k */
+  def Lemma006SuffixIntroduce(p: String, q: String, A: String, B: String) = {
+    require(p.bigLength < q.bigLength && A + p == B + q)
+    val (k, l) = splitFromEnd(q, p.bigLength)
+    k
+  } ensuring { (k: String) => q == k + p && A == B + k }
+
+/*
+If A + B == C + A and |A| <= |C|, then there exists k1 and k2 such that
+C = k1+k2
+B = k2+k1
+A = k1
+*/
+  def LemmaCommutation1(A: String, B: String, C: String) = {
+    require(A + B == C + A && C.bigLength >= A.bigLength)
+    val (k1, k2) = split(C, A.bigLength)
+    (k1, k2)
+  } ensuring {
+    k => C == k._1 + k._2 && B == k._2 + k._1 && A == k._1
+  }
+
+/*
+  If A + B == C + A and |A| <= |C|, then there exists k1 and k2 such that
+  C = k1+k2
+  B = k2+k1
+  A = k1+k2+k1+..+k2+k1
+
+2)    | C |    Ap      |
+1)| C |       A        |
+1)|       A        | B |
+2)    |    Ap      |
+
+  Hence Ap commutes with C and yields B
+*/
+  def LemmaPreCondCommutation1(A: String, B: String, C: String): Boolean = {
+    require(A + B == C + A && C.bigLength < A.bigLength)
+    val (cp, ap) = split(A, C.bigLength)
+    cp == C &&
+    A == C + ap
+  } holds
+  
+  def LemmaPreCondCommutation2(A: String, B: String, C: String): Boolean = {
+    require(A + B == C + A && C.bigLength < A.bigLength)
+    val (app, bp) = splitFromEnd(A, B.bigLength)
+    bp == B &&
+    app + bp == A
+  } holds
+  
+  def LemmaPreCondCommutation(A: String, B: String, C: String): Boolean = {
+    require(A + B == C + A && C.bigLength < A.bigLength)
+    val (cp, ap) = split(A, C.bigLength)
+    val (app, bp) = splitFromEnd(A, B.bigLength)
+    LemmaPreCondCommutation1(A, B, C) &&
+    LemmaPreCondCommutation2(A, B, C) &&
+    C + ap == A &&
+    (C + ap) + B == C + A &&
+    LemmaAssociativity(C, ap, B) &&
+    C + (ap + B) == C + A &&
+    LemmaLeftSimplification(ap + B, A, C) &&
+    ap + B == A &&
+    C + ap == ap + B
   } holds
 
+  def LemmaCommutation2(A: String, B: String, C: String): (String, String, String) = {
+    require(A + B == C + A)
+    if(C.bigLength >= A.bigLength) {
+      val (k1, k2) = LemmaCommutation1(A, B, C)
+      (k1, k2, A)
+    } else {
+      val (c, ap) = split(A, C.bigLength)
+      if (LemmaPreCondCommutation(A, B, C)) {
+        val k = LemmaCommutation2(ap, B, C)
+        (k._1, k._2, k._1 + k._2 + k._3) 
+      } else (A, B, C) // Dummy, used because assert, assume, require do not work above.
+    }
+  } ensuring {
+    k => C == k._1 + k._2 && B == k._2 + k._1 && A == k._3
+  }
+
+  // Other lemmas
   def LemmaCommutative$0(A: String, B: String, n: BigInt): Boolean = {
     require(n >= 2)
     B + power(A, n) == (B + A) + power(A, n-1)
@@ -96,16 +189,11 @@ There exist a constant k such that q = p k and A = k B*/
     (A + power(A, n-1)) + B == power(A, n) + B
   } holds
   
-  @library
-  def LEmamjk(A: String, B: String, n: BigInt): Boolean = {
-     (B + power(A, n) == power(A, n) + B)
-  } holds
-  
   def emptyStringCommutes(A: String): Boolean = {
     A + "" == "" + A
   } holds
 
-  def LemmaCommutative(A: String, B: String, n: BigInt): Boolean = {
+  /*def LemmaCommutative(A: String, B: String, n: BigInt): Boolean = {
     require(A + B == B + A && n >= 0)
     B + power(A, n) == power(A, n) + B because {
       if(n == 0) power(A, 0) == "" && emptyStringCommutes(B)
@@ -120,9 +208,9 @@ There exist a constant k such that q = p k and A = k B*/
         power(A, n) + B
       } qed
     }
-  } holds
+  } holds*/
   
-  def LemmaCommutativeWOProofDSL(A: String, B: String, n: BigInt): Boolean = {
+  def LemmaCommutative(A: String, B: String, n: BigInt): Boolean = {
     require(A + B == B + A && n >= 0)
     (
       if(n == 0) power(A, 0) == "" && emptyStringCommutes(B)
@@ -138,23 +226,6 @@ There exist a constant k such that q = p k and A = k B*/
     ) && B + power(A, n) == power(A, n) + B 
   } holds
   
-  /*@induct
-  def LemmaCommutative2(A: String, B: String, n: BigInt): Boolean = {
-    require(A + B == B + A && n >= 0)
-    power(A + B, n) == power(A, n) + power(B, n) because {
-      if(n == 0) trivial
-      else if(n == 1) trivial
-      else {
-        power(A, n) == A + power(A, n - 1)
-        power(B, n) == B + power(B, n - 1)
-        power(A + B, n)
-          == A + B + power(A + B, n - 1)
-          == A + B + power(A, n - 1) + power(B, n - 1)
-          == B + A + power(A, n - 1) + power(B, n - 1)
-          == B + power(A, n) + power(B, n - 1)
-      }
-    }
-  } holds*/
 /*
 
 
