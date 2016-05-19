3166db7b8243f37090854cfb2bb3e1ff778d9ff9
Mikaël Mayer <3601079>
2016-05-19 17:35:50 +0200

    Added the affine mapping.

--- Transducers.scala
+++ Transducers.scala
@@ -18,46 +18,61 @@ object YDT {
     require(size >= 1 && content.length == size)
   }
   
+  // We make it total, so there is no option type here.
   val initState = BigInt(1)
-  case class UNARY_YDT(nbStates: BigInt, symbols: Set[Symbol], delta: (BigInt, Symbol) => Option[List[UnaryExpr]] ) {
+  case class UNARY_YDT(nbStates: BigInt, symbols: Set[Symbol], delta: (BigInt, Symbol) => List[UnaryExpr] ) {
     require(nbStates >= 1)
-    def evaluate(t: Tree): Option[BigInt] = {
+    def evaluate(t: Tree): BigInt = {
       evaluate(t, initState)
     }
     
     // [[t]]q == [[t.symbol (t.children ...) ]]q == [[delta(q, t.symbol)]]_M ([[t.children(0)]]q, ... [[t.children(t.symbol.arity-1)]]q)
-    def evaluate(t: Tree, q: BigInt): Option[BigInt] = {
+    def evaluate(t: Tree, q: BigInt): BigInt = {
       require(q >= 1 && q <= nbStates)
-      delta(q, t.symbol) match {
-        case None() => None()
-        case Some(unaryExprs) => eval(unaryExprs, t, 0)
-      }
+      if(isConsistent(delta(q, t.symbol), t.symbol))
+        eval(delta(q, t.symbol), t, 0)
+      else BigInt(0)
     }
+    
+    def isConsistent(u: UnaryExpr, x: Symbol): Boolean = u match {
+      case I => true
+      case S(s, childIndex) => s >= 1 && s <= nbStates && childIndex >= 1 && childIndex <= x.arity
+    }
+    
+    // Checks if an expression can handle the given symbol.
+    def isConsistent(T: List[UnaryExpr], x: Symbol): Boolean = T match {
+      case Nil() => true
+      case Cons(t, tail) => isConsistent(t, x) && isConsistent(tail, x)
+    }
+    
+    // We can build up consistency
+    def LemmaIsConsistent(u: UnaryExpr, T: List[UnaryExpr], x: Symbol): Boolean = {
+      require(isConsistent(u, x) && isConsistent(T, x))
+      isConsistent(Cons(u, T), x)
+    } holds
+    
+    def LemmaIsConsistent2(T: Cons[UnaryExpr], x: Symbol): Boolean = {
+      require(isConsistent(T, x))
+      isConsistent(T.tail, x)
+    } holds
 
     // Corresponds to [[T]]_M x
-    def eval(T: List[UnaryExpr], x: Tree, acc: BigInt): Option[BigInt] = T match {
-      case Nil() => Some(acc)
-      case Cons(I, tail) => eval(tail, x, acc + BigInt(1))
-      case Cons(S(childState, childIndex), tail) => 
-        if(childIndex < 0 || childIndex >= x.symbol.arity)
-          None()
-        else {
-          val child = x.children(childIndex)
-          delta(childState, child.symbol) match {
-            case None() => None()
-            case Some(unaryExprs) =>
-              eval(unaryExprs, child,0) match {
-                case None() => None()
-                case Some(res) =>
-                  eval(tail, x, acc + res)
-              }
-          }
-        }
+    def eval(T: List[UnaryExpr], x: Tree, acc: BigInt): BigInt = {
+      require(isConsistent(T, x.symbol))
+      T match {
+        case Nil() => acc
+        case Cons(I, tail) => eval(tail, x, acc + BigInt(1))
+        case Cons(S(childState, childIndex), tail) => 
+          val child = x.children(childIndex - 1)
+          val unaryExprs = delta(childState, child.symbol)
+          val res = if(isConsistent(unaryExprs, child.symbol)) eval(unaryExprs, child, 0) else BigInt(0)
+          eval(tail, x, acc + res)
+      }
     }
     
     // [[t]] for any state.
-    def evaluateForAll(t: Tree): Vector[Option[BigInt]] = {
-      def recEval(s: BigInt, t: Tree, acc: List[Option[BigInt]]): List[Option[BigInt]]= {
+    def evaluateForAll(t: Tree): Vector[BigInt] = {
+      def recEval(s: BigInt, t: Tree, acc: List[BigInt]): List[BigInt]= {
         require(s >= 0 && s + acc.length == nbStates)
         if(s <= 0) {
           acc
@@ -65,10 +80,38 @@ object YDT {
           recEval(s - 1, t, evaluate(t, s)::acc)
         }
       } ensuring {
-        (res: List[Option[BigInt]]) => res.length == s + acc.length
+        (res: List[BigInt]) => res.length == s + acc.length
       }
       val l = recEval(nbStates, t, Nil())
       Vector(nbStates, l)
+    }
+    
+    // Used by evaluateForAllUnaryExpr
+    def evaluateForAllUnaryExpr2(xi: Vector[BigInt], j: BigInt): BigInt = {
+      require(xi.size == nbStates && j >= 1 && j <= nbStates)
+      if(xi.size == xi.content.length) {
+        xi.content(j - 1)
+      } else error[BigInt]("Should not happen")
+    }
+    
+    // [[T]] x
+    def evaluateForAllUnaryExpr(f: Symbol, T: List[UnaryExpr], x: Vector[Vector[BigInt]]): BigInt = {
+      require(x.size == f.arity && x.content.forall(xi => xi.size == nbStates) && isConsistent(T, f))
+      T match {
+        case Nil() => BigInt(0)
+        case Cons(I, tail) => evaluateForAllUnaryExpr(f, tail, x) + BigInt(1)
+        case c@Cons(S(j, i), tail) => 
+          if(LemmaIsConsistent2(c, f) && isConsistent(tail, f)) {
+          if(i >= 1 && i <= f.arity && j >= 1 && j <= nbStates) {
+          if (ListSpecs.applyForAll(x.content, i-1, (xi: Vector[BigInt]) => xi.size == nbStates) &&
+            x.content(i-1).size == nbStates) {
+            val xi: Vector[BigInt] = x.content(i-1)
+            val res = evaluateForAllUnaryExpr2(xi, j)
+            res + evaluateForAllUnaryExpr(f, tail, x)
+          } else error[BigInt]("Should not happen")
+          } else error[BigInt]("Should not happen")
+          } else error[BigInt]("Should not happen")
+      }
     }
   }
 }
